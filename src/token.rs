a920ed5967f9540f003c511be33931bb65927ec5
piapot <piapot.hou@outlook.com>
2023-09-10 23:03:20 +0800

    feat: complete simple tokenizer

--- src/token.rs
+++ src/token.rs
@@ -4,7 +4,7 @@ pub enum TokenKind {
     Linefeed,
     Digit,
     Word,
-    Symbol,
+    Punctuation,
     Unknown,
 }
 
--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -16,12 +16,45 @@ impl Tokenizer {
         }
     }
 
+    pub fn scan(&mut self) -> Vec<Token> {
+        let mut tokens = vec![];
+
+        while let Some(token) = self.next() {
+            tokens.push(token);
+        }
+
+        tokens
+    }
+
     pub fn next(&mut self) -> Option<Token> {
         if let Some(char) = self.input.get(self.cursor) {
             match *char {
                 b'\n' => self.tokenize_linefeed(),
-                c if c.is_ascii_whitespace() && c != b'\n' => self.tokenize_whitespace(),
-                _ => unreachable!(),
+                c if c != b'\n' && c.is_ascii_whitespace() => {
+                    let f = |c: u8| c.is_ascii_whitespace() && c != b'\n';
+                    self.take_while(TokenKind::Whitespace, f)
+                }
+                c if c.is_ascii_digit() => {
+                    let f = |c: u8| c.is_ascii_digit();
+                    self.take_while(TokenKind::Digit, f)
+                }
+                c if c.is_ascii_alphabetic() => {
+                    let f = |c: u8| c.is_ascii_alphabetic();
+                    self.take_while(TokenKind::Word, f)
+                }
+                c if c.is_ascii_punctuation() => {
+                    let f = |c: u8| c.is_ascii_punctuation();
+                    self.take_while(TokenKind::Punctuation, f)
+                }
+                _ => {
+                    let f = |c: u8| {
+                        !(c.is_ascii_whitespace()
+                            || c.is_ascii_digit()
+                            || c.is_ascii_alphabetic()
+                            || c.is_ascii_punctuation())
+                    };
+                    self.take_while(TokenKind::Unknown, f)
+                }
             }
         } else {
             None
@@ -39,19 +72,8 @@ impl Tokenizer {
         })
     }
 
-    fn tokenize_whitespace(&mut self) -> Option<Token> {
+    fn take_while(&mut self, kind: TokenKind, f: fn(u8) -> bool) -> Option<Token> {
         let start = self.cursor;
-        let chars = self.take_while(|c| c.is_ascii_whitespace() && c != b'\n');
-
-        chars.map(|chars| {
-            let kind = TokenKind::Whitespace;
-            let line = self.line;
-            let end = start + chars.len();
-            Token::new(kind, chars, line, [start, end])
-        })
-    }
-
-    fn take_while(&mut self, f: fn(u8) -> bool) -> Option<Vec<u8>> {
         let mut chars = vec![];
 
         while let Some(char) = self.input.get(self.cursor) {
@@ -63,9 +85,13 @@ impl Tokenizer {
             }
         }
 
-        match chars.len() == 0 {
-            false => Some(chars),
-            true => None,
+        let length = chars.len();
+
+        if length != 0 {
+            let end = start + length;
+            Some(Token::new(kind, chars, self.line, [start, end]))
+        } else {
+            None
         }
     }
 }
